0c7b26e15b862874a17de6a0449e0d8bc2ab5402
Fabio Koiti <fabio.koiti03@gmail.com>
2021-02-23 01:04:49 +0000

    implementei a classe produtos e instanciei os produtos (objetos)

--- main.py
+++ main.py
@@ -13,13 +13,13 @@ class Pessoa:
     print ("SUA MENSAGEM FOI ENVIADA COM SUCESSO!")
 
 class Endereco:
-  def __init__ (self, logradouro, numero, bairro, complemento, cidade, cep)
-  self.logradouro = logradouro
-  self.numero = numero
-  self.bairro = bairro
-  self.complemento = complemento
-  self.cidade = cidade
-  self.cep = cep
+  def __init__ (self, logradouro, numero, bairro, complemento, cidade, cep):
+    self.logradouro = logradouro
+    self.numero = numero
+    self.bairro = bairro
+    self.complemento = complemento
+    self.cidade = cidade
+    self.cep = cep
   def cadastrar_endereco (self):
     self.Endereco()
   def alterar_endereco (self):
@@ -163,4 +163,37 @@ class Carrinho:
       return True
     else:
       return False
+
+class Produtos:
+  def __init__(self, preco_produto, id_produto, tipo_ProdutoSalgado):
+    self.preco_produto = preco_produto
+    self.id_produto = id_produto
+    self.tipo_ProdutoSalgado = tipo_ProdutoSalgado
+  def cadastrar_produto(self):
+    self.produto = input ("DIGITE O NOME DO PRODUTO A SER CADASTRADO: ")
+    print("PRODUTO CADASTRADO COM SUCESSO!")
+  def alterar_produto(self):
+    self.produto = input ("DIGITE O NOVO PRODUTO: ")
+    print("PRODUTO ALTERADO COM SUCESSO!")
+  def adicionar_produto_estoque(self):
+    produto_estoque = input ("DIGITE O NOME DO PRODUTO A SER ACRESCENTADO NO ESTOQUE: ")
+    print("PRODUTO ADICIONADO COM SUCESSO!")
+  def remover_produto_estoque(self):
+    produto_estoque = None
+
+sushi_tradicional = Produtos(3.00, 1, True)
+hosomaki = Produtos(3.00, 2, True)
+makizushi = Produtos(3.50, 3, True)
+temaki = Produtos(5.00, 4, True)
+urumaki = Produtos(3.50, 5, True)
+niguiri = Produtos(8.00, 6, True)
+sashimi = Produtos(5.00, 7, True)
+hot_filadelfia = Produtos(4.00, 8, True)
+tempura = Produtos(5.00, 9, True)
+yakisoba_tradicional = Produtos(47.90, 10, True)
+yakisoba_vegano = Produtos(48.00, 11, True)
+harumaki = Produtos(10.00, 12, True)
+moyashi = Produtos(20.00, 13, True)
+mochi = Produtos(5.00, 14, False)
+manju = Produtos(6.00, 15, False)
 
